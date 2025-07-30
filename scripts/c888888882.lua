762b8e0fb9196b816f467fe8bdc5bc150e2bdcca
COZZILLA <connor.rigby2005@outlook.com>
2025-07-30 20:52:11 +0100

    Add files via upload

--- scripts/c888888882.lua
+++ scripts/c888888882.lua
@@ -2,81 +2,84 @@
 --Created by COZZILLA
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate and equip
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1)
+    --Equip procedure
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_EQUIP)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
+    c:RegisterEffect(e1)
 
-	--Only equipped monster can attack (twice)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_EXTRA_ATTACK)
-	e2:SetValue(1)
-	c:RegisterEffect(e2)
+    --Allow equipped monster to attack twice
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_EQUIP)
+    e2:SetCode(EFFECT_EXTRA_ATTACK)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
 
-	--Other monsters cannot attack
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_CANNOT_ATTACK)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(s.atktg)
-	c:RegisterEffect(e3)
+    --Prevent other monsters from attacking
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_ATTACK)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetTargetRange(LOCATION_MZONE,0)
+    e3:SetTarget(s.atktg)
+    c:RegisterEffect(e3)
 
-	--Destruction Substitute
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_EQUIP)
-	e4:SetCode(EFFECT_DESTROY_REPLACE)
-	e4:SetTarget(s.reptg)
-	e4:SetOperation(s.repop)
-	c:RegisterEffect(e4)
+    --Destruction replacement
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_EQUIP)
+    e4:SetCode(EFFECT_DESTROY_REPLACE)
+    e4:SetTarget(s.reptg)
+    e4:SetOperation(s.repop)
+    c:RegisterEffect(e4)
 end
 
---Cost: Send 1 card from hand
+--Cost: Send 1 card from hand to Graveyard
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
 
---Equip target: LIGHT Galaxy monster
+--Only target LIGHT Galaxy monsters
 function s.filter(c)
-	return c:IsFaceup() and c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_LIGHT)
+    return c:IsFaceup() and c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 end
+
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Equip(tp,c,tc)
-	else
-		Duel.SendtoGrave(c,REASON_EFFECT)
-	end
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if not c:IsRelateToEffect(e) then return end
+    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        Duel.Equip(tp,c,tc)
+    end
 end
 
--- Prevent others from attacking
+--Other monsters can't attack
 function s.atktg(e,c)
-	return c~=e:GetHandler():GetEquipTarget()
+    return c~=e:GetHandler():GetEquipTarget()
 end
 
--- Destruction substitute
+--Destruction substitute
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ec=e:GetHandler()
-	local tc=ec:GetEquipTarget()
-	if chk==0 then return tc and tc:IsReason(REASON_BATTLE+REASON_EFFECT) and ec:IsAbleToGrave() end
-	return true
+    local ec=e:GetHandler()
+    local tc=ec:GetEquipTarget()
+    if chk==0 then return tc and tc:IsOnField()
+        and tc:IsReason(REASON_BATTLE+REASON_EFFECT)
+        and not ec:IsStatus(STATUS_DESTROY_CONFIRMED) end
+    return true
 end
+
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
+    Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
 end
