8ff26b847e4e37139fb276bae0ef6f2138eac439
COZZILLA <connor.rigby2005@outlook.com>
2025-07-30 20:45:03 +0100

    Add files via upload

--- scripts/c666777888.lua
+++ scripts/c666777888.lua
@@ -1,47 +1,82 @@
--- Galactica Mining
--- Script by ChatGPT
-
+--Eternal Mirage
+--Created by COZZILLA
 local s,id=GetID()
-
 function s.initial_effect(c)
-    -- Activate effect: send 2 face-up Galaxy monsters you control to GY, then add 1 Spell from GY to hand
+    --Activate and equip
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetCategory(CATEGORY_EQUIP)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1,id)
+    e1:SetCost(s.cost)
     e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
+    e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
+
+    --Only equipped monster can attack (twice)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_EQUIP)
+    e2:SetCode(EFFECT_EXTRA_ATTACK)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+
+    --Other monsters cannot attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_ATTACK)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetTargetRange(LOCATION_MZONE,0)
+    e3:SetTarget(s.atktg)
+    c:RegisterEffect(e3)
+
+    --Destruction Substitute
+    local e4=Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_EQUIP)
+    e4:SetCode(EFFECT_DESTROY_REPLACE)
+    e4:SetTarget(s.reptg)
+    e4:SetOperation(s.repop)
+    c:RegisterEffect(e4)
 end
 
-function s.tgfilter(c)
-    return c:IsFaceup() and c:IsRace(RACE_GALAXY) and c:IsAbleToGrave()
+--Cost: Send 1 card from hand
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
 
-function s.thfilter(c)
-    return c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+--Equip target: LIGHT Galaxy monster
+function s.filter(c)
+    return c:IsFaceup() and c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
-
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
-        return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_MZONE,0,2,nil)
-            and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        Duel.Equip(tp,c,tc)
+    else
+        Duel.SendtoGrave(c,REASON_EFFECT)
     end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_MZONE)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_MZONE,0,2,2,nil)
-    if Duel.SendtoGrave(g,REASON_EFFECT)==2 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local sg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-        if #sg>0 then
-            Duel.SendtoHand(sg,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-tp,sg)
-        end
-    end
+-- Prevent others from attacking
+function s.atktg(e,c)
+    return c~=e:GetHandler():GetEquipTarget()
+end
+
+-- Destruction substitute
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local ec=e:GetHandler()
+    local tc=ec:GetEquipTarget()
+    if chk==0 then return tc and tc:IsReason(REASON_BATTLE+REASON_EFFECT) and ec:IsAbleToGrave() end
+    return true
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
 end
